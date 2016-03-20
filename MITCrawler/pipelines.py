84478f380dcda88a606267c242d8cb5d31527a5d
Дмитрий Басавин <basavind@gmail.com>
2016-03-20 11:41:24 +0300

    format csv output

--- MITCrawler/pipelines.py
+++ MITCrawler/pipelines.py
@@ -6,6 +6,40 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class MitcrawlerPipeline(object):
-    def process_item(self, item, spider):
-        return item
+from scrapy import signals
+from scrapy.contrib.exporter import CsvItemExporter
+
+class CSVPipeline(object):
+
+  def __init__(self):
+    self.files = {}
+
+  @classmethod
+  def from_crawler(cls, crawler):
+    pipeline = cls()
+    crawler.signals.connect(pipeline.spider_opened, signals.spider_opened)
+    crawler.signals.connect(pipeline.spider_closed, signals.spider_closed)
+    return pipeline
+
+  def spider_opened(self, spider):
+    file = open('%s_items.csv' % spider.name, 'w+b')
+    self.files[spider] = file
+    self.exporter = CsvItemExporter(file)
+    self.exporter.fields_to_export = ['section',
+                                      'section_url',
+                                      'course',
+                                      'course_url',
+                                      'material_section',
+                                      'material_section_url',
+                                      'material',
+                                      'material_url']
+    self.exporter.start_exporting()
+
+  def spider_closed(self, spider):
+    self.exporter.finish_exporting()
+    file = self.files.pop(spider)
+    file.close()
+
+  def process_item(self, item, spider):
+    self.exporter.export_item(item)
+    return item
